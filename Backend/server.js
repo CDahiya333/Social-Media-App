423811104a5c4b261c1d36872400a520ad44f2d8
Chirag Dahiya <dahiyachirag67@gmail.com>
2025-02-10 23:59:26 +0530

    User Controls Added

--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,13 @@
 import dotenv from "dotenv";
 //Current Working Directory; TWitterClone
-dotenv.config({path:"Backend/.env"}); //Relative Path to Backend/.env
-
+dotenv.config({ path: "Backend/.env" }); //Relative Path to Backend/.env
 
 import express from "express";
 import authRoutes from "./routes/authRoutes.js";
 import connectMongoDB from "./server/DB.js";
 import cookieParser from "cookie-parser";
-
+import userRoutes from "./routes/userRoutes.js";
+import { v2 as cloudinary } from "cloudinary";
 // Debugging
 // console.log("Loaded MONGO_URI:", process.env.MONGO_URI);
 // console.log("Current Working Directory:", process.cwd());
@@ -19,8 +19,10 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use("/api/auth", authRoutes);
 
+//Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 //Serving Running Get request on Home Page
 app.get("/", (req, res) => {
@@ -29,4 +31,8 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Listening to PORT:${port}`);
   connectMongoDB();
+  cloudinary.config({
+    api_key: process.env.CLOUDINARY_URL,
+  });
+  console.log("Cloudinary Connected");
 });
