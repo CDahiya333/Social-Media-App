d8c13ba47138bcbac9297c95944ab78934ed4b5d
Chirag Dahiya <dahiyachirag67@gmail.com>
2025-02-26 20:11:22 +0530

    viteonfig.js reconfigured

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3000",
-        changeOrigin: true,
-      },
-    },
-  },
+  plugins: [react()], // Remove tailwindcss() because it's not a Vite plugin
 });
